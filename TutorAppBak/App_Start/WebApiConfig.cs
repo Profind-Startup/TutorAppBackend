82033e07e944abefd214f78d4818149e763e81e2
xt9102 <eariasce@everis.com.pe>
2019-09-10 17:34:03 -0500

    Cambios para formato json

--- TutorAppBak/App_Start/WebApiConfig.cs
+++ TutorAppBak/App_Start/WebApiConfig.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 namespace TutorAppBak
 {
     public static class WebApiConfig
@@ -10,10 +11,26 @@ namespace TutorAppBak
         public static void Register(HttpConfiguration config)
         {
             // Configuración y servicios de API web
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
 
+            var formatter = GlobalConfiguration.Configuration.Formatters.Where(f => f is System.Net.Http.Formatting.JsonMediaTypeFormatter).FirstOrDefault();
+            if (!formatter.SupportedMediaTypes.Any(mt => mt.MediaType == "text/plain"))
+                formatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/plain"));
+
+
+            //Ignorar self-references
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
+               Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+
+            //Ignorar valores null
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+
+
+          //  config.Formatters.Insert(0, new TextMediaTypeFormatter());
             // Rutas de API web
             config.MapHttpAttributeRoutes();
-
+            // Rutas de API web
+            
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
