64c0b93e8b4282120ce5a5302d8fbb7a3757247e
u201314147 <u201314147@upc.edu.pe>
2019-11-11 13:15:36 -0500

    agregado controlador reservaciones

--- TutorAppBackend/Controllers/api/ReservationsController.cs
+++ TutorAppBackend/Controllers/api/ReservationsController.cs
@@ -16,7 +16,40 @@ namespace TutorAppBackend.Controllers.api
     {
         public ReservationsController() : base() { }
 
-        
+        //GET: api/Students/5/Reservations
+        [Route("api/Reservations/{id}/details")]
+        public ReservationDetails GetReservationDetail(int id)
+        {
+            ReservationDetails reservationDetail;
+
+            Reservation reservation = _context.Reservation.Find(id);
+
+            Tutor tutor = _context.Tutor.Find(reservation.tutor_id);
+
+            Student student = _context.Student.Find(reservation.student_id);
+
+            User userSt = _context.User.Find(student.user_id);
+            User userTt = _context.User.Find(tutor.user_id);
+
+            Subject subject = _context.Subject.Find(reservation.subject_id);
+
+            reservationDetail = new ReservationDetails();
+
+            reservationDetail.id = reservation.id;
+
+            reservationDetail.reservation_date = reservation.reservation_date.ToString();
+
+            reservationDetail.reservation_time_start = reservation.reservation_time_start.ToString();
+            reservationDetail.reservation_time_end = reservation.reservation_time_end.ToString();
+            reservationDetail.studentNames = userSt.names;
+            reservationDetail.studentNames = userSt.last_names;
+            reservationDetail.tutorNames = userTt.names;
+            reservationDetail.tutorLastnames = userTt.last_names;
+            reservationDetail.subjectname = subject.name;
+            reservationDetail.totalpayment = "0";
+          
+            return reservationDetail;
+        }
 
         // GET: api/Reservations
         public IQueryable<Reservation> GetReservation()
